c292e27e61dd3b4b96f4ca6421c06a62b5f4da57
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2024-04-15 21:27:09 +0200

    added correct loader

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { register } from "node:module";
 import { pathToFileURL  } from "node:url";
 
-register("node_modules/@gxcloud/typescript/lib/loader.js", pathToFileURL("./"));
+register("@gxcloud/typescript/lib/loader.js", pathToFileURL("./"));
