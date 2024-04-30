1bfea1340f1307de83e614d2bff0c8380b75c815
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2024-04-30 20:21:34 +0200

    allow imports without file name extension

--- src/loader.js
+++ src/loader.js
@@ -1,14 +1,23 @@
 import { transform } from "esbuild";
-import  { extname } from "node:path";
+import { extname } from "node:path";
 
 const typescriptExtensions = new Set([".ts", ".tsx"]);
 
 export async function resolve(specifier, context, nextResolve) {
-  const fileNameExtension = extname(specifier);
+  let fileNameExtension = extname(specifier);
 
-  if (!typescriptExtensions.has(fileNameExtension)) {
-    return nextResolve(specifier);
+  // if file name extension is not in import
+  if (
+    !fileNameExtension &&
+    (specifier.includes("./") || specifier.includes("../"))
+  ) {
+    specifier += ".ts";
+    fileNameExtension = ".ts";
   }
+
+    if (!typescriptExtensions.has(fileNameExtension)) {
+      return nextResolve(specifier);
+    }
 
   const { url } = await nextResolve(specifier);
 
@@ -24,7 +33,7 @@ export async function load(url, context, nextLoad) {
     return nextLoad(url);
   }
 
-  const rawSource = (await nextLoad(url, { ...context, format: "moudle" }))
+  const rawSource = (await nextLoad(url, { ...context, format: "module" }))
     .source;
 
   const { code: transpiledSource } = await transform(rawSource, {
